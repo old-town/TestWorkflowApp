589b673ece3ac637483243f3906b22ef6d8e8e05
Malofeykin Andrey <malofeykin@platformasoft.ru>
2015-12-08 15:38:36 +0300

    sm-107 Альтернативная реализация модуля через preDispatch

--- config/application.config.php
+++ config/application.config.php
@@ -15,6 +15,7 @@ return array(
         //'AsseticBundle',
         'Application',
         'OldTown\\Workflow\\ZF2',
+        'OldTown\\Workflow\\ZF2\\PreDispatch',
         'OldTown\\Workflow\\ZF2\View',
         'OldTown\Workflow\ZF2\ServiceEngine',
         'OldTown\\Workflow\\Designer\\Server',
